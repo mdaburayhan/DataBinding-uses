e1ea705d94e7cae48d1a100189bfead2612805f0
Md Abu Rayhan <mdaburayhan833@gmail.com>
2025-05-21 18:25:26 +0600

    Showing data to a textview using DataBinidng

---
+++ app/src/main/java/com/arsoft/databindingapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.arsoft.databindingapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+
+import com.arsoft.databindingapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding activityMainBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Person p1 = new Person("Jack", "jack@gmail.com");
+        activityMainBinding = DataBindingUtil.setContentView(
+                this,
+                R.layout.activity_main
+        );
+
+        activityMainBinding.setPerson(p1);
+
+
+    }
+}
